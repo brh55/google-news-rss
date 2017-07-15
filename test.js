6d7abf3c18ab571e27924ef4a565902ec28ba0a1
Adalberto Diniz Bueno Junior <adalbueno@MacBook-Air-de-Adalberto.local>
2017-07-15 11:28:22 -0300

    Fixing mock and tests

--- test.js
+++ test.js
@@ -5,12 +5,12 @@ import GoogleNewsClient from '.';
 
 nock('https://news.google.com/news')
 	.get('/section')
-	.query({q: 'food', output: 'rss', num: 10})
+	.query({q: 'food', output: 'rss', num: 10, hl: 'en'})
 	.reply(200, rssMock);
 
 nock('https://news.google.com/news')
 		.get('/section')
-		.query({q: 'food', output: 'rss', num: 30})
+		.query({q: 'food', output: 'rss', num: 30, hl: 'en'})
 		.reply(200, rssMock);
 
 const googleNews = new GoogleNewsClient();
@@ -19,20 +19,21 @@ test('Searches the news and returns formatted articles', async t => {
 	t.plan(3);
 
 	const expectedArticle1 = {
-		title: '14 killed as soldiers clash over drought food aid in Somalia - ABC News',
-		link: 'http://news.google.com/news/url?sa=t&fd=R&ct2=us&usg=AFQjCNHGjodT8ktfZmfvbTpDMab0g84E4w&clid=c3a7d30bb8a4878e06b80cf16b898331&cid=52779524260870&ei=8P86WYDQD87jhAHyqJfQDA&url=http://abcnews.go.com/International/wireStory/14-killed-soldiers-clash-drought-food-aid-somalia-47937452',
-		pubDate: 'Fri, 09 Jun 2017 13:49:25 GMT',
-		description: '14 killed as soldiers clash over drought food aid in Somalia',
-		publisher: 'ABC News'
+		title: 'Your pick: World\'s 50 best foods - CNN',
+		link: 'http://news.google.com/news/url?sa=t&fd=R&ct2=us&usg=AFQjCNEDNjfEObRdKmRlsbRO4BSuFFC2hg&clid=c3a7d30bb8a4878e06b80cf16b898331&ei=rCJqWYjEINKMzgKc_YDgCQ&url=http://www.cnn.com/travel/article/world-best-foods-readers-choice/index.html',
+		pubDate: 'Wed, 12 Jul 2017 12:04:47 GMT',
+		description: 'Your pick: World&apos;s 50 best foods',
+		publisher: 'CNN',
+		thumbnailUrl: '//t0.gstatic.com/images?q=tbn:ANd9GcQtGI39Y_w847WipM0d9UBrcxj41HrNLEL_lDfnl0sw-BqkdHEN1U2d-xNF4rXw3B4PTumy1y28'
 	};
 
 	const expectedArticle2 = {
-		title: 'Rodent feces, bugs found during Muskegon-area April food inspections - MLive.com',
-		link: 'http://news.google.com/news/url?sa=t&fd=R&ct2=us&usg=AFQjCNEj73ZFzrJsREMf4xAffwkeuUZfkA&clid=c3a7d30bb8a4878e06b80cf16b898331&ei=8P86WYDQD87jhAHyqJfQDA&url=http://www.mlive.com/news/muskegon/index.ssf/2017/06/rodent_feces_mold_in_cooler_fo.html',
-		pubDate: 'Fri, 09 Jun 2017 12:14:32 GMT',
-		description: 'Rodent feces, bugs found during Muskegon-area April food inspections',
-		publisher: 'MLive.com',
-		thumbnailUrl: '//t2.gstatic.com/images?q=tbn:ANd9GcRzm0aUDI6tl9pyN4yN5hRxvNxMl7bnLwqVDdNcCO7mEzOjqtFcHkkiW1r_O7EqNnxFTca77i0D'
+		title: 'Best Korean dishes: 40 foods we can\'t live without - CNN',
+		link: 'http://news.google.com/news/url?sa=t&fd=R&ct2=us&usg=AFQjCNEeo82SxlrQZP5SzjugGtJHFt36_g&clid=c3a7d30bb8a4878e06b80cf16b898331&ei=rCJqWYjEINKMzgKc_YDgCQ&url=http://www.cnn.com/travel/article/best-korean-dishes/index.html',
+		pubDate: 'Wed, 12 Jul 2017 15:54:41 GMT',
+		description: 'Best Korean dishes: 40 foods we can&apos;t live without',
+		publisher: 'CNN',
+		thumbnailUrl: '//t0.gstatic.com/images?q=tbn:ANd9GcQCDS0sTjw9VGQ8fJRIlNhCjhe871Vz7IIQ1VQncaK7NrSNpOy5WUKL5CYQxVJ4aUjpQ6VT7Qpp'
 	};
 
 	const articles = await googleNews.search('food');
